98a40644314893997a49f16fe79757ac815eebd5
k-krtn <58627649+k-krtn@users.noreply.github.com>
2020-01-13 17:09:40 +0900

    changes routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  resources :messages
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "messages#index"
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messages#index'
 end
 
